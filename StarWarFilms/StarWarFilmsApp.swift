45aa9358f41f85ab00b989c2e6786f825525509f
phonglh-2668 <le.hung.phong@sun-asterisk.com>
2024-02-05 21:19:33 +0700

    Initial Commit

---
+++ StarWarFilms/StarWarFilmsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarWarFilmsApp.swift
+//  StarWarFilms
+//
+//  Created by le.hung.phong on 05/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct StarWarFilmsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
